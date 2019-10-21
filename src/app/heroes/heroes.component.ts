ffea56a7303019d1640d83a66eb7ec33c52c85ac
anguadi <anguadir@gmail.com>
2019-10-21 12:12:45 +0200

    Links inside app added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,20 +9,9 @@ import { HeroService } from '../hero.service'
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-
   heroes: Hero[];
 
-  selectedHero: Hero;
-
   constructor(private heroService: HeroService) { }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero
-  }
 
   ngOnInit() {
     this.getHeroes()
